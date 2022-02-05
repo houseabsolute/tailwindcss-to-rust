55274a3333f8b8f044374cfab18085054dbb4ac2
Dave Rolsky <autarch@urth.org>
2022-02-05 15:07:47 -0600

    Add macros package

---
+++ macros/examples/main.rs
@@ -0,0 +1,3 @@
+mod css;
+
+fn main() {}
---
+++ macros/src/to_option_vec_string.rs
@@ -0,0 +1,65 @@
+// This code was copied from Seed (https://github.com/seed-rs/seed) and
+// modified.
+//
+// Copyright 2019 DavidOConnor <david.alan.oconnor@gmail.com>
+//
+// Licensed under the MIT license only.
+
+//! Trait to make the `C!` and `M!` macros work with many types.
+//!
+//! You need to make sure this trait is imported by any code that wants to use
+//! the `C!` and `M!` macros.
+
+pub trait ToOptionVecString {
+    fn to_option_vec_string(self) -> Option<Vec<String>>;
+}
+
+// ------ Implementations ------
+
+impl<T: ToOptionVecString + Clone> ToOptionVecString for &T {
+    fn to_option_vec_string(self) -> Option<Vec<String>> {
+        self.clone().to_option_vec_string()
+    }
+}
+
+// --- Texts ---
+
+impl ToOptionVecString for String {
+    fn to_option_vec_string(self) -> Option<Vec<String>> {
+        Some(vec![self])
+    }
+}
+
+impl ToOptionVecString for &str {
+    fn to_option_vec_string(self) -> Option<Vec<String>> {
+        Some(vec![self.to_string()])
+    }
+}
+
+// --- Containers ---
+
+impl<T: ToOptionVecString> ToOptionVecString for Option<T> {
+    fn to_option_vec_string(self) -> Option<Vec<String>> {
+        self.and_then(ToOptionVecString::to_option_vec_string)
+    }
+}
+
+impl<T: ToOptionVecString> ToOptionVecString for Vec<T> {
+    fn to_option_vec_string(self) -> Option<Vec<String>> {
+        let classes = self
+            .into_iter()
+            .filter_map(ToOptionVecString::to_option_vec_string)
+            .flatten();
+        Some(classes.collect())
+    }
+}
+
+impl<T: ToOptionVecString + Clone> ToOptionVecString for &[T] {
+    fn to_option_vec_string(self) -> Option<Vec<String>> {
+        let classes = self
+            .iter()
+            .filter_map(ToOptionVecString::to_option_vec_string)
+            .flatten();
+        Some(classes.collect())
+    }
+}
