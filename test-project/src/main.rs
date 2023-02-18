c3fe51c9cfa17530df862e8cf55c72b2936ddd97
Dave Rolsky <autarch@urth.org>
2023-02-18 12:27:29 -0600

    Update test project code to work with new generator

--- test-project/src/main.rs
+++ test-project/src/main.rs
@@ -1,30 +1,44 @@
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(hello).service(
-            fs::Files::new("/", "./public")
-                .show_files_listing()
-                .use_last_modified(true),
-        )
-    })
-    .bind(("0.0.0.0", 7373))?
-    .run()
-    .await
+mod generated;
+
+use generated::{C, M};
+use tailwindcss_to_rust_macros::{ToOptionVecString, C, M};
+
+fn main() {
+    println!("{}", C::bg::bg_rose_500);
+    println!("{}", M::hover);
+    println!(
+        "{}",
+        C![M![M::hover, C::bg::bg_rose_500], C::typ::text_white],
+    );
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body(html!(
-       <html>
-         <head>
-            <title>"Stuff a ndthigns"</title>
-            <link rel="stylesheet" href="/assets/tailwind_compiled.css" />
-        </head>
-         <body>
-            <div class={{ C.bg.bg_rose_500 }}>"Is this "<i>{hey("italicised?")}</i></div>
-            <div class={{ M.hover }}:{{ C.spc.mt_0 }}></div>
-            <div class="{{ M.hover }}:{{ C.bg.bg_rose_500 }} {{ C.bg.bg_rose_800 }}">...</div>
-         </body>
-       </html>
-    ))
-}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new().service(hello).service(
+//             fs::Files::new("/", "./public")
+//                 .show_files_listing()
+//                 .use_last_modified(true),
+//         )
+//     })
+//     .bind(("0.0.0.0", 7373))?
+//     .run()
+//     .await
+// }
+
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body(html!(
+//        <html>
+//          <head>
+//             <title>"Stuff a ndthigns"</title>
+//             <link rel="stylesheet" href="/assets/tailwind_compiled.css" />
+//         </head>
+//          <body>
+//             <div class={{ C::bg.bg_rose_500 }}>"Is this "<i>{hey("italicised?")}</i></div>
+//             <div class={{ M::hover }}:{{ C::spc.mt_0 }}></div>
+//             <div class="{{ M::hover }}:{{ C::bg.bg_rose_500 }} {{ C::bg.bg_rose_800 }}">...</div>
+//          </body>
+//        </html>
+//     ))
+// }
--- test-project/tailwind.config.js
+++ test-project/tailwind.config.js
@@ -14,9 +14,9 @@ module.exports = {
             .replaceAll("_", "-");
         };
 
-        let one_class_re = "{{\\s*C\\.[a-z0-9_]+\\.([a-z0-9_]+)\\s*}}";
+        let one_class_re = "\\bC::[a-z0-9_]+::([a-z0-9_]+)\\b";
         let class_re = new RegExp(one_class_re, "g");
-        let one_mod_re = "{{\\s*M\\.([a-z0-9_]+)\\s*}}";
+        let one_mod_re = "\\bM::([a-z0-9_]+)\\b";
         let mod_re = new RegExp(one_mod_re + ":" + one_class_re, "g");
 
         let classes = [];
